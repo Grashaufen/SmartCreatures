94e44bf9a9c1ed8e8e9fabbe6e62ddb7d7bad7e5
Grashaufen <jannes_oltmanns@hotmail.de>
2021-02-24 19:35:00 +0100

    fixed groben Schnitzer

--- src/NeuralNetwork/NeuralNetworkMutational.cpp
+++ src/NeuralNetwork/NeuralNetworkMutational.cpp
@@ -14,7 +14,7 @@ void NeuralNetworkMutational::Mutate(float highest, float lowest)
 	// every connection adds a random number to its weight between heighest and lowest
 	for(unsigned int i = 0; i < m_hiddenLayers.size(); i++)
 		for(unsigned int j = 0; j < m_hiddenLayers[i]->size(); j++)
-			for(unsigned int k = 0; k < m_hiddenLayers[i]->at(k)->getConnections().size(); k++)
+			for(unsigned int k = 0; k < m_hiddenLayers[i]->at(j)->getConnections().size(); k++)
 				m_hiddenLayers[i]->at(j)->getConnections().at(k)->SetWeight(
 						m_hiddenLayers[i]->at(j)->getConnections().at(k)->getWeight() + Math::frand(lowest, highest));
 
